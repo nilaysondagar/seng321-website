ccab1b6678fb2ae7773c225254a18c511cd7a828
nilay-freshworks <nilay@freshworks.io>
2020-01-19 15:54:39 -0800

    README Update - Updated README with super super basic running instructions

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # seng321-website
-SENG321 group website for displaying RFPs and basic company info 
+
+## Running the App
+
+After cloning the repo, you can run the app by running the following:
+
+```sh
+cd seng321-website
+npm install
+npm run start
+```
+
+Then open [http://localhost:3000/](http://localhost:3000/) to see your app.<br>
+
+When you’re ready to deploy to production, create a minified bundle with `npm run build`.
---
+++ build/precache-manifest.e0aacab8c2566b8d831ffd1474507fa3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebf485a8a00a1af948dba1f228e95092",
+    "url": "/seng321/index.html"
+  },
+  {
+    "revision": "1e6a70370ad5457de1b6",
+    "url": "/seng321/static/css/main.adb52991.chunk.css"
+  },
+  {
+    "revision": "62475b0ef2b7d208a4ec",
+    "url": "/seng321/static/js/2.4ba28aa2.chunk.js"
+  },
+  {
+    "revision": "1e6a70370ad5457de1b6",
+    "url": "/seng321/static/js/main.529c27a2.chunk.js"
+  },
+  {
+    "revision": "d9433d77b9b06e5dc9d0",
+    "url": "/seng321/static/js/runtime~main.3cd38c15.js"
+  },
+  {
+    "revision": "50cb83420b155eb67c80901afd2a996b",
+    "url": "/seng321/static/media/cityImage.50cb8342.jpg"
+  },
+  {
+    "revision": "bb18bdbbef437b2d50518db5a8292c94",
+    "url": "/seng321/static/media/emailImage.bb18bdbb.png"
+  },
+  {
+    "revision": "5d6ea38a769498dfc19fe6389d14db39",
+    "url": "/seng321/static/media/facebookLogoImage.5d6ea38a.png"
+  },
+  {
+    "revision": "655b17f2f5ee4cdb68b6c319abbb91a4",
+    "url": "/seng321/static/media/ferryImage.655b17f2.jpeg"
+  },
+  {
+    "revision": "a60ff83234175c154aface649af7f279",
+    "url": "/seng321/static/media/memeImage.a60ff832.jpg"
+  },
+  {
+    "revision": "fc386bcac0a2b0c93b0d6cbd5830b890",
+    "url": "/seng321/static/media/nilayHeadshot.fc386bca.jpg"
+  },
+  {
+    "revision": "cff24bbfc77345d40aa4577e33308d83",
+    "url": "/seng321/static/media/rfp.cff24bbf.pdf"
+  },
+  {
+    "revision": "0579c30ebb9b9d78fa0d32cf83c19bbb",
+    "url": "/seng321/static/media/snowplowImage.0579c30e.jpg"
+  },
+  {
+    "revision": "91c116d019ccc8c7279dbed729d54826",
+    "url": "/seng321/static/media/twitterLogoImage.91c116d0.png"
+  }
+]);
